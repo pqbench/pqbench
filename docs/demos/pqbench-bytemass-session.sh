2faa8c3c00234ff298c6bc890101eb4e5c1686ee
roman <roman1001001@gmail.com>
2026-09-19 08:51:25 +0200

    docs: add byte-mass visual demos

---
+++ docs/demos/pqbench-bytemass-session.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+set -eu
+printf '\033[1;32m$\033[0m pqbench bytemass alltypes_dictionary.parquet\n'
+sleep 1
+docker run --rm --platform "$PQBENCH_DEMO_PLATFORM" \
+    -v "$PWD:/src:ro" \
+    -v "$PWD/.docker-data/deltabench-example:/demo:ro" \
+    -v "$PQBENCH_DEMO_TARGET:/src/target" \
+    -w /src rust:1.91.1-bookworm \
+    target/debug/pqbench bytemass /demo/alltypes_dictionary.parquet
+sleep 2
